c0c58ac22d79ddf5e21fbf9902edfff3bc609f8a
Justin Bennett <fortitude1981@gmail.com>
2016-06-12 22:02:42 -0500

    inital commit

---
+++ app/app.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');  //es6 destructuring syntax; same as var Route = require('react-router').Route etc....
+var Main = require('Main');
+var Nav = require('Nav');
+
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();  //fire up foundation
+
+//App css
+require('style!css!sass!applicationStyles')
+
+//react-router
+ReactDOM.render(                    //most common react DOM method to render to screen
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+
+    </Route>
+  </Router>,        //jsx greeter tag from component
+  document.getElementById('app')
+);
---
+++ app/components/Main.jsx
@@ -0,0 +1,31 @@
+//render default app
+var React = require('react');
+var Nav = require('Nav');
+// var Main = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <Nav/>
+//         <h2>Main Component</h2>
+//         {this.props.children}
+//       </div>
+//     );
+//   }
+// });
+
+//use this instead of above
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Nav/>
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
