aa43364df711911b15265630e8d44b2c55068919
Justin Bennett <fortitude1981@gmail.com>
2016-07-08 19:45:27 -0500

    support foundation settings

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,6 +5,8 @@ var CountdownForm = React.createClass({
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
 
+    console.log('input count', $('input').length); //ability to use jQuery
+
     if (strSeconds.match(/^[0-9]*$/)) {  //if it valid,
       this.refs.seconds.value = '';     //clear the field and
       this.props.onSetCountdown(parseInt(strSeconds, 10)); //then call parent function 'onSetCountdown'
